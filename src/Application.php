5a412e94526ef6cf98bddfe5aa1a5556b9e927d3
leo108 <leo108@qq.com>
2017-04-22 19:00:35 +0800

    v0.2

--- src/Application.php
+++ src/Application.php
@@ -1,24 +1,31 @@
 <?php
-namespace ConsoleQrCode;
+
+namespace Leo108\ConsoleQrCode;
 
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Input\InputInterface;
-use ConsoleQrCode\Command\ConvertCommand;
+use Leo108\ConsoleQrCode\Command\ConvertCommand;
 
-class Application extends BaseApplication {
-    protected function getCommandName(InputInterface $input) {
+class Application extends BaseApplication
+{
+    protected function getCommandName(InputInterface $input)
+    {
         return 'convert';
     }
 
-    protected function getDefaultCommands() {
-        $defaultCommands = parent::getDefaultCommands();
+    protected function getDefaultCommands()
+    {
+        $defaultCommands   = parent::getDefaultCommands();
         $defaultCommands[] = new ConvertCommand();
+
         return $defaultCommands;
     }
 
-    public function getDefinition(){
+    public function getDefinition()
+    {
         $inputDefinition = parent::getDefinition();
         $inputDefinition->setArguments();
+
         return $inputDefinition;
     }
 }
--- src/Command/ConvertCommand.php
+++ src/Command/ConvertCommand.php
@@ -1,30 +1,33 @@
 <?php
-namespace ConsoleQrCode\Command;
 
+namespace Leo108\ConsoleQrCode\Command;
+
+use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Symfony\Component\Process\ProcessBuilder;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 use PHPQRCode\QRcode;
 
-class ConvertCommand extends Command {
-    protected function configure() {
+class ConvertCommand extends Command
+{
+    protected function configure()
+    {
         $this->setName('convert')
-             ->addOption('text', null, InputOption::VALUE_REQUIRED, 'The text you want to convert to QRcode')
-             ->addOption('lr', null, InputOption::VALUE_REQUIRED, 'The left and right padding you want the QRcode has', 1)
-             ->addOption('tb', null, InputOption::VALUE_REQUIRED, 'The top and bottom padding you want the QRcode has', 1)
-             ->addOption('stdin', null, InputOption::VALUE_NONE, 'Read the text from SDTIN');
+            ->addOption('text', null, InputOption::VALUE_REQUIRED, 'The text you want to convert')
+            ->addOption('lr', null, InputOption::VALUE_REQUIRED, 'The left and right padding', 1)
+            ->addOption('tb', null, InputOption::VALUE_REQUIRED, 'The top and bottom padding', 1)
+            ->addOption('stdin', null, InputOption::VALUE_NONE, 'Read the text from SDTIN');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
         $lrPadding = $input->getOption('lr');
         $tbPadding = $input->getOption('tb');
-        if($input->getOption('stdin')) {
+        if ($input->getOption('stdin')) {
             $text = '';
-            while($line = fgets(STDIN)){
+            while ($line = fgets(STDIN)) {
                 $text .= $line;
             }
         } else {
@@ -32,8 +35,7 @@ class ConvertCommand extends Command {
         }
 
         if (empty($text)) {
-            $output->getErrorOutput()->writeln('<error>Convert text cannot be empty</error>');
-            return;
+            throw new InvalidArgumentException('Convert text cannot be empty');
         }
 
         $map = array(
@@ -41,24 +43,10 @@ class ConvertCommand extends Command {
             1 => '<blackc>  </blackc>',
         );
         $this->initStyle($output);
-        $text   = QRcode::text($text);
-        $length = strlen($text[0]);
-
-        $screenSize = $this->getTTYSize();
-        if(!$screenSize) {
-            $output->getErrorOutput()->writeln('<comment>Get Screen Size Failed</comment>');
-        } else {
-            list($maxLines, $maxCols) = $screenSize;
-            $qrCols = 2 * ($length + $lrPadding * 2);
-            $qrLines = count($text) + $tbPadding * 2;
-            if($qrCols > $maxCols || $qrLines > $maxLines){
-                $output->getErrorOutput()->writeln('<error>Max Lines/Columns Reached</error>');
-                return;
-            }
-        }
-
-        $paddingLine = str_repeat($map[0], $length + $lrPadding * 2) . "\n";
-        $after = $before = str_repeat($paddingLine, $tbPadding);
+        $text        = QRcode::text($text);
+        $length      = strlen($text[0]);
+        $paddingLine = str_repeat($map[0], $length + $lrPadding * 2)."\n";
+        $after       = $before = str_repeat($paddingLine, $tbPadding);
         $output->write($before);
         foreach ($text as $line) {
             $output->write(str_repeat($map[0], $lrPadding));
@@ -71,30 +59,11 @@ class ConvertCommand extends Command {
         $output->write($after);
     }
 
-    private function initStyle(OutputInterface $output) {
+    protected function initStyle(OutputInterface $output)
+    {
         $style = new OutputFormatterStyle('black', 'black');
         $output->getFormatter()->setStyle('blackc', $style);
         $style = new OutputFormatterStyle('white', 'white');
         $output->getFormatter()->setStyle('whitec', $style);
-    }
-
-    private function getTTYSize() {
-        if(!posix_isatty(STDOUT)){
-            return false;
-        }
-        $ttyName = posix_ttyname(STDOUT);
-
-        $builder = new ProcessBuilder();
-        $process = $builder->setPrefix('stty')->setArguments(array('-f', $ttyName, 'size'))->getProcess();
-        try {
-            $process->mustRun();
-        } catch (ProcessFailedException $e) {
-            return false;
-        }
-        $output = $process->getOutput();
-        if(!preg_match('~^(\d+)\s+(\d+)$~', $output, $match)) {
-            return false;
-        }
-        return array($match[1], $match[2]);
     }
 }
